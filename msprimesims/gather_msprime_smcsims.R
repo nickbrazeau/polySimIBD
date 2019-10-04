25e46641f02a5375766d45745f145f6b63e7d241
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-10-04 16:42:52 +0100

    gather up msprime sims

--- msprimesims/gather_msprime_smcsims.R
+++ msprimesims/gather_msprime_smcsims.R
@@ -34,57 +34,20 @@ wfsmpl5 <- lapply(wfsmpl5, read_msprime_sims) %>%
 wfsims <- rbind.data.frame(wfsmpl2, wfsmpl3, wfsmpl5) %>%
   dplyr::mutate(model = "discrete wright fisher")
 
-#....................................
-# smc from msprime
-#....................................
-smcsmpl2 <- list.files("~/Desktop/msprimesims/smc/smplsize2/", full.names = T)
-smcsmpl2 <- lapply(smcsmpl2, read_msprime_sims) %>%
-  dplyr::bind_rows()
-
-smcsmpl3 <- list.files("~/Desktop/msprimesims/smc/smplsize3/", full.names = T)
-smcsmpl3 <- lapply(smcsmpl3, read_msprime_sims) %>%
-  dplyr::bind_rows()
-
-smcsmpl5 <- list.files("~/Desktop/msprimesims/smc/smplsize5/", full.names = T)
-smcsmpl5 <- lapply(smcsmpl5, read_msprime_sims) %>%
-  dplyr::bind_rows()
-
-
-smcsims <- rbind.data.frame(smcsmpl2, smcsmpl3, smcsmpl5) %>%
-  dplyr::mutate(model = "smc")
 
 #....................................
-# smcprime from msprime
-#....................................
-smcprimesmpl2 <- list.files("~/Desktop/msprimesims/smcprime/smplsize2/", full.names = T)
-smcprimesmpl2 <- lapply(smcprimesmpl2, read_msprime_sims) %>%
-  dplyr::bind_rows()
-
-smcprimesmpl3 <- list.files("~/Desktop/msprimesims/smcprime/smplsize3/", full.names = T)
-smcprimesmpl3 <- lapply(smcprimesmpl3, read_msprime_sims) %>%
-  dplyr::bind_rows()
-
-smcprimesmpl5 <- list.files("~/Desktop/msprimesims/smcprime/smplsize5/", full.names = T)
-smcprimesmpl5 <- lapply(smcprimesmpl5, read_msprime_sims) %>%
-  dplyr::bind_rows()
-
-smcprimesims <- rbind.data.frame(smcprimesmpl2, smcprimesmpl3, smcprimesmpl5) %>%
-  dplyr::mutate(model = "smcprime")
-
-#....................................
-# come together
+# write out
 #....................................
 
-msprimesims <- rbind.data.frame(wfsims, smcsims, smcprimesims)
-# write out for Bob
-saveRDS(msprimesims, file = "~/Desktop/msprimesims.rds")
+
+saveRDS(wfsims, file = "~/Desktop/msprimesims.rds")
 
 #....................................
 # make plots
 #....................................
 
 
-msprimeplot <- msprimesims %>%
+wfsimsplot <- wfsims %>%
   dplyr::filter(time2parent != 0) %>% # terminal branch points
   dplyr::group_by(simnum, treeindex) %>%
   dplyr::arrange(simnum, treeindex, time2parent) %>%
@@ -93,10 +56,10 @@ msprimeplot <- msprimesims %>%
   ggplot() +
   ggridges::geom_density_ridges(aes(x = time2parent, y = ET, fill = ET, color = ET), alpha = 0.6) +
   xlim(0, 1000) +
-  facet_wrap(model ~ smplsize) +
+  facet_wrap(. ~ smplsize) +
   theme_bw() +
   theme(legend.position = "none")
 
-jpeg("~/Desktop/msprime_results.jpg", height = 8, width = 8, res = 250, units = "in")
-plot(msprimeplot)
+jpeg("~/Desktop/msprime_dtwf_results.jpg", height = 8, width = 8, res = 250, units = "in")
+plot(wfsimsplot)
 graphics.off()
