1b8a7ad1a0f0eba543193f35aeb61a8e9fbea7a3
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-10-04 14:31:15 +0100

    LL submission script

---
+++ msprimesims/LLsim_sWF.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --time=5-00:00:00
+#SBATCH --mem=49512
+#SBATCH --mail-type=all
+#SBATCH --mail-user=nbrazeau@med.unc.edu
+
+Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/polySimIBD"); source("msprimesims/sim_forward_WF")'
