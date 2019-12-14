f98766f9e1172e57340498478bcc4b4eb109bc76
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-12-14 11:32:59 +0000

    bash script for run on LL

---
+++ R_ignore/NatComms_VerityAB2019_Sims/999-LL_run_Verity_natcomms_sims.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --time=36:00:00
+#SBATCH --mem=49512
+#SBATCH --mail-type=all
+#SBATCH --mail-user=nbrazeau@med.unc.edu
+
+Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/polySimIBD"); source("/R_ignore/NatComms_VerityAB2019_Sims/run_Verity_natcomms_sims.R")'
