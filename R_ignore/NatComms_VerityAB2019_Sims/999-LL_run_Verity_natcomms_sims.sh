0996a4f230d3352a607255ffaf38505df179538b
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-12-14 11:47:50 +0000

    🙈

--- R_ignore/NatComms_VerityAB2019_Sims/999-LL_run_Verity_natcomms_sims.sh
+++ R_ignore/NatComms_VerityAB2019_Sims/999-LL_run_Verity_natcomms_sims.sh
@@ -6,4 +6,4 @@
 #SBATCH --mail-type=all
 #SBATCH --mail-user=nbrazeau@med.unc.edu
 
-Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/polySimIBD"); source("/R_ignore/NatComms_VerityAB2019_Sims/run_Verity_natcomms_sims.R")'
+Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/polySimIBD"); source("R_ignore/NatComms_VerityAB2019_Sims/run_Verity_natcomms_sims.R")'
