81cbeb048d49a175709795d91d9536efef44dbd6
nickbrazeau <nbrazeau@med.unc.edu>
2020-12-21 15:53:45 -0500

    spat feat model

--- tests/testthat/test-model_runs.R
+++ tests/testthat/test-model_runs.R
@@ -5,7 +5,7 @@ test_that("model will run", {
   #migr_dist_mat <- matrix(runif(9), 3, 3)
   migr_dist_mat <- matrix(0, 3, 3)
   diag(migr_dist_mat) <- 1
-  ret <- polySimIBD::sim_swf(pos = sample(1:1e3, size = 50), 
+  swf <- polySimIBD::sim_swf(pos = sample(1:1e3, size = 50), 
                              N = N, 
                              m = m, 
                              mean_coi = mean_coi, 
@@ -14,6 +14,10 @@ test_that("model will run", {
                              rho = 1e-2, 
                              tlim = 5,
                              verbose = FALSE)
+  
+  ARG <- polySimIBD::get_arg(swf = swf, host_index = c(1,2))
+  
+  
 })
 
 ret$coi
@@ -23,3 +27,15 @@ unlist(ret$parent_host1[[5]])
 unlist(ret$parent_host2[[5]])
 unlist(ret$parent_haplo1[[5]])
 unlist(ret$parent_haplo2[[5]])
+
+
+
+swf <- polySimIBD::sim_swf(pos = sample(1:1e3, size = 50), 
+                           N = 20, 
+                           m = 0.5, 
+                           mean_coi = 1, 
+                           migr_dist_mat = matrix(1,1,1), 
+                           dist_scalar = 1,
+                           rho = 1e-2, 
+                           tlim = 5,
+                           verbose = FALSE)
