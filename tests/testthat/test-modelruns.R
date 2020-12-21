3c2a95af214f77bdc835626cc1136fa70d77c4bc
nickbrazeau <nbrazeau@med.unc.edu>
2020-12-21 15:24:06 -0500

    add tests

---
+++ tests/testthat/test-modelruns.R
@@ -0,0 +1,9 @@
+test_that("model runs", {
+  swfsim <- polySimIBD::sim_swf(pos = sample(1:1e3, size = 100),
+                                N = 20,
+                                m = 0.5,
+                                rho = 5e-2,
+                                mean_coi =  2,
+                                tlim = 10)
+  ARG <- polySimIBD::get_arg(swfsim, host_index = list(1:2))
+})
