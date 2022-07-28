3bdb0ec857af5be6f7080a54149884d8c0e6789f
nickbrazeau <nbrazeau1@gmail.com>
2022-07-28 18:48:59 -0400

    new IBD btwn fxn

--- R/get_results.R
+++ R/get_results.R
@@ -5,9 +5,9 @@
 setClass("bvtree",
          slots=list(c="numeric", t="numeric", z="numeric"))
 
-
+#------------------------------------------------
 #' @title Extract Effective COI by Loci from SWF Simulation for a Single Host
-#' @inheritParams get_swf
+#' @inheritParams get_arg
 #' @details Only accepts a single host 
 #' @description TODO 
 #' @return vector of effective COI by loci
@@ -28,9 +28,9 @@ get_effective_coi <- function(swf, host_index = NULL) {
   return(effCOI)
 }
 
-
-
-#' @inheritParams get_swf
+#------------------------------------------------
+#' @title Calculate Within-Host IBD 
+#' @inheritParams get_arg
 #' @details Only accepts a single host 
 #' @description TODO
 #' @return double of within-host IBD
@@ -45,7 +45,7 @@ get_within_IBD <- function(swf, host_index = NULL) {
   # get connections
   conn <- purrr::map(arg, "c")
   # get within host IBD per loci 
-  numerator <- purrr::map_dbl(conn, function(x){sum(x != -1)})
+  numerator <- sapply(conn, function(x){sum(x != -1)})
   # -1 here for self comparison
   denom <-  (swf$coi[[host_index]]-1)
   # under SNP vs PSMC (Li/Durbin model) don't know begin and end, so treat as missing info
@@ -77,11 +77,50 @@ get_conn_intervals <- function(uniqueconn, allconn){
   return(as.numeric(mint))
 }
 
+#------------------------------------------------
+#' @title Calculate Between Host (pairwise) IBD 
+#' @inheritParams bvtree
+#' @param int internal identification of root indices 
+#' @description Internal function: Recursively loop through tree starting with root to find haplo-indices in the "bvtrees c slot" that are connected
+#' @noMd
+#' @noRd
+get_conn_from_root <- function(root, c) {
+  # init
+  tree <- newroot <- root
+  #   
+  while(length(newroot) != 0) {
+    newroot <- which(c %in% (newroot-1)) # match c value and update new root 
+    tree <- c(tree, newroot)
+  }
+  # out
+  return(tree)
+}
 
+#------------------------------------------------
+#' @title Find relevant connections for within IBD calculation
+#' @inheritParams bvtree
+#' @description Internal function: subset bvtree "c" slot to haplo-indices that contain a between sample connections and therefore contribute to within-sample IBD in a pairwise comparison 
+#' @noMd
+#' @noRd
 
-#' @title Between Host (pairwise) IBD 
+get_withinIBD_bvtree_subset <- function(c, coi1, coi2) {
+  # find roots
+  roots <- which(c == -1)
+  # subset to tree based on roots 
+  subset_trees <- lapply(roots, get_bvtree_from_root, c = c)
+  # get btwn conn
+  btwnconn <- which(c[(coi1+1):(coi2+coi1)] %in% 0:(coi1-1)) + coi1
+  
+  # drop spmls w/ no btwn 
+  subset_trees <- subset_trees[ sapply(subset_trees, function(x, btwn){any(x %in% btwn)}, btwn = btwnconn) ]
+  # out
+  return(sort(unique(unlist(subset_trees))))
+}
+
+#------------------------------------------------
+#' @title Calculate Between Host (pairwise) IBD 
 #' @description Given an object \code{swf}, ***
-#' @inheritParams get_swf
+#' @inheritParams get_arg
 #' @description ***
 #' @details  Only accepts a pair of hosts (i.e. pairwise). Ignores mutations as interrupting IBD segments. 
 #' @return ***
@@ -101,18 +140,16 @@ get_pairwise_ibd <- function(swf, host_index = NULL) {
   # subset to unique loci for speed 
   conn <- purrr::map(arg, "c")
   uniconn <- unique(conn)
-  # store indices
+  # store indices for unique loci locations
   conn_indices <- get_conn_intervals(uniqueconn = uniconn, allconn = conn)
-  # define arguments
-  haplo_index <- mapply(function(x) 1:x, swf$coi[host_index], SIMPLIFY = FALSE)
-  host_haplo_cnt <- mapply(length, haplo_index)
+  # define arguments for fast cpp function 
   argums <- list(conn = uniconn, 
-                 host_haplo_cnt = host_haplo_cnt)
+                 host_haplo_cnt = swf$coi[host_index])
   
   # we define btwn_pairwise_ibd as: 
   # (n_{coal-btwn} + n_{coal-win-btwn}) / (n_{strains1} * n{strains2})
   # where w/in coal share a btwn coal 
-
+  
   # pass to efficient C++ function for quick between tree look up 
   # to determine between host IBD
   output_raw <- calc_between_IBD_cpp(argums)
@@ -121,27 +158,41 @@ get_pairwise_ibd <- function(swf, host_index = NULL) {
   # catch if no btwn, no w/in 
   if(sum(output_raw == 0)) { return(0)}
   
-  # within host IBD 
- host1_wibd <- polySimIBD:::get_perloci_within_IBD(swf, host_index = host_index[1])
- host2_wibd <- polySimIBD:::get_perloci_within_IBD(swf, host_index = host_index[2])
- 
- # numerator of btwn and w/in IBD
-  numerator <- # TODO 
- 
- # under SNP vs PSMC (Li/Durbin model) don't know begin and end, so treat as missing info
- wi <- diff(swf$pos)/sum(diff(swf$pos))
+  # subset to relevant haploindices in bvtrees for correct w/in IBD calculation 
+  # i.e. only within IBD that has a pairwise connection, or btwn smpl connection, contributes to 
+  # overall calculation of pairwise IBD 
+  relconn <- lapply(uniconn, get_withinIBD_bvtree_subset, 
+                    coi1 = swf$coi[host_index][1],
+                    coi2 = swf$coi[host_index][2])
+  wunniconn <- mapply(function(x, y){return(x[y])}, x = uniconn, y = relconn, SIMPLIFY = F)
+  
+  # need to still respect COI, which is [ ] below 
+  # within host IBD for smpl 1
+  win_smpl1 <- mapply(x = wunniconn, y = relconn, function(x,y){
+    z <- x[ y %in% 1:swf$coi[host_index][1] ]
+    return(sum(z != -1)) }, SIMPLIFY = T)
+  # within host IBD for smpl 1
+  win_smpl2 <- mapply(x = wunniconn, y = relconn, function(x,y){
+    z <- x[ y %in% swf$coi[host_index][1]:sum(swf$coi[host_index]) ]
+    return(sum(z != -1)) }, SIMPLIFY = T)
+  
+  
   # TODO 
+  # need to expand out conn intervals from above
+  # conn_indices
   
+  # numerator of btwn and w/in IBD
+  numerator <- output_raw + win_smpl1 + win_smpl2
   
-  
-  # out
-  out <- # TODO 
-  return(out)
+  # under SNP vs PSMC (Li/Durbin model) don't know begin and end, so treat as missing info
+  wi <- diff(swf$pos)/sum(diff(swf$pos))
+  # weighted average
+  return( sum( (numerator / prod(swf$coi[host_index]))*wi ) )
 }
 
 
 
-
+#------------------------------------------------
 #' Extract haplotypes from ARG
 #' @param arg set of bvtrees
 #' @return hapmat numeric matrix; a matrix of mutliallelic haplotypes for each parasite considered. Loci are in
