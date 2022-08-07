fafdb16e6dbf1bc2404cf4aa341d80c3593bb106
nickbrazeau <nbrazeau1@gmail.com>
2022-08-07 23:30:31 +0300

    update man

--- R/main.R
+++ R/main.R
@@ -42,7 +42,8 @@ plot_bvtree <- function(bvtree, group, tlim){
     dplyr::mutate(time = ifelse(t == -1, Inf, t),
                   haplotype = seq(from = 0, to = (length(t)-1), by = 1),
                   conn = ifelse(t == Inf, NA, c),
-                  membership = factor(group))
+                  membership = factor(paste0("host", group),
+                                      levels = unique(paste0("host", group))))
   # marginal tree 
   make_marginal_tree_plot <- function(x, tlim){
     # get nonroots
@@ -53,21 +54,22 @@ plot_bvtree <- function(bvtree, group, tlim){
       geom_segment(data = nonroots,
                    aes(x = haplotype, xend = conn,
                        y = time, yend = time), # horizontal lines
-                   size = 1) +
+                   show.legend = F, size = 1) +
       geom_segment(data = x,
                    aes(x = haplotype, xend = haplotype,
-                       y = 0, yend = time, group = membership),
+                       y = 0, yend = time, color = membership),
                    size = 1) +  # vertical lines
       xlab("Haplotypes") + ylab("Generations") +
       theme_bw() +
       theme(axis.title = element_text(family = "Helvetica", face = "bold", size = 16),
             axis.text.x = element_blank(),
             axis.text.y = element_text(family = "Helvetica", face = "bold", size = 16),
-            legend.position = "none",
+            legend.position = "right",
             panel.grid = element_blank(),
             panel.border = element_blank(),
             axis.line = element_line(color = "#bdbdbd", size = 1.1))   + 
-      ylim(0, tlim)
+      ylim(0, tlim) +
+      ggplot2::scale_color_viridis_d("Host (by COI)")
     
     return(plotObj)
   }
