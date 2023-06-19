2992811745ad264049d89fc0cdfc1442e094b355
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-06-19 18:25:57 -0400

    update test

--- tests/testthat/test-plotObjs.R
+++ tests/testthat/test-plotObjs.R
@@ -7,15 +7,9 @@ testthat::test_that("Plotting functions work", {
                              rho = 1e-2, 
                              tlim = 2)
   ARG <- polySimIBD::get_arg(swf = swf, host_index = c(1,2))
-  hapmat <- extract_haplotype_matrix(arg = ARG)
-  get_coi(swf)
   # plot coal trees
   plotObj <- plot_coalescence_trees(ARG = ARG, loci = 1)
   testthat::expect_true( any(class(plotObj) %in% "ggplot" ))
     
-  #plot hapmap
-  barcodeplotObj <- plot_barcodes(hapmat, coi = swf$coi[1:2])
-  testthat::expect_true( any(class(barcodeplotObj) %in% "ggplot" ))
-
   })
 
