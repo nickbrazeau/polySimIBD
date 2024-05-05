1c03cd57b57ceb734bb042ffd57b93345b692d1c
Nicholas Brazeau <nbrazeau@med.unc.edu>
2024-05-05 16:15:39 -0400

    update docs

--- tests/testthat/test-conversion.R
+++ tests/testthat/test-conversion.R
@@ -9,5 +9,13 @@ testthat::test_that("conversion returns newick string for a simple tree", {
 
 
 testthat::test_that("conversion returns newick string for multi-overlapping tree", {
- #TODO
+
+  # make custom bvtree
+  bvtree_ex <- new("bvtree", "c" = c(-1,-1,0,-1,1), "t" = c(-1,-1,4,-1,6), "z" = c(2,3,0,4,1))
+  out <- bvtreeToNewick(bvtree = bvtree_ex, tlim = 10) 
+  # make sure liftover is correct
+  testthat::expect_equal(out,
+                         "((Node0:4,Node2:4):6,(Node1:6,Node4:6):4,Node3:10);")
+  
+  
 })
